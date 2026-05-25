779626fff1e425d9bbf53546feb0295795955398
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-05-25 21:47:56 +0100

    Fixed vertex coordinates of the triangle

--- assets/shaders/triangle.hlsl
+++ assets/shaders/triangle.hlsl
@@ -8,13 +8,13 @@ VSOut VSMain(uint VertexID : SV_VERTEXID) {
     VSOut output = (VSOut)0;
 
     if (VertexID == 0) { 
-        output.Pos = float4(0.0, -0.5, 0.0, 1.0); 
+        output.Pos = float4(0.0, 0.5, 0.0, 1.0); 
         output.Color = float3(1.0, 0.0, 0.0); 
     } else if (VertexID == 1) { 
-        output.Pos = float4(0.5, 0.5, 0.0, 1.0); 
+        output.Pos = float4(0.5, -0.5, 0.0, 1.0); 
         output.Color = float3(0.0, 1.0, 0.0); 
     } else if (VertexID == 2) { 
-        output.Pos = float4(-0.5, 0.5, 0.0, 1.0); 
+        output.Pos = float4(-0.5, -0.5, 0.0, 1.0); 
         output.Color = float3(0.0, 0.0, 1.0); 
     }
     
