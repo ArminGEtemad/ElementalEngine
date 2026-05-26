771116a5710ac4b553277d12f99d568e715243b3
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-05-26 22:37:21 +0100

    Adding choice for API
    I decided that I give the user the choice of what backend they want to use and not depending on macros.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,51 @@
-#include "core/ApplicationDX12.hpp"
-#include "core/ApplicationVK.hpp"
+#include "rhi/Device.hpp"
+#include "rhi/RHICommon.hpp"
 
 #include <cstdlib>
 #include <iostream>
 
-#define USE_DX12 true
+using namespace elementalEngine;
+using namespace elementalEngine::RHI;
 
 int main() {
-#if USE_DX12
-  elementalEngine::ApplicationDX12 app{};
-#else
-  elementalEngine::ApplicationVK app{};
-#endif
+  static constexpr int WIDTH{1000};
+  static constexpr int HEIGHT{800};
+
+  GraphicsAPI selectedBackend;
+  int choice;
+
+  std::cout << "-----------------------------------\n";
+  std::cout << "   .:: ELEMENTAL ENGINE ::.\n";
+  std::cout << "-----------------------------------\n";
+  std::cout << "Select Backend:\n";
+  std::cout << "  [1] Vulkan 1.3\n";
+  std::cout << "  [2] DirectX 12\n";
+  std::cout << "Enter your choice: ";
+
+  std::cin >> choice;
+  if (choice == 1) {
+    selectedBackend = GraphicsAPI::Vulkan;
+    std::cout << "Vulkan 1.3 Backend has been selected...\n";
+  } else if (choice == 2) {
+    selectedBackend = GraphicsAPI::DirectX12;
+    std::cout << "DirectX 12 Backend has been selected...\n";
+  } else {
+    selectedBackend = GraphicsAPI::Vulkan;
+    std::cerr << "Invalid choice! Default to Vulkan 1.3...\n";
+  }
 
   try {
-    app.run();
+    WindowHandling window{WIDTH, HEIGHT, "Elemental Engine"};
+    DeviceConfig config{};
+    config.enableValidationLayers = true;
+    std::unique_ptr<Device> device(
+        RHIFilter::createDevice(selectedBackend, config, window));
+
+    std::cout << "main loop starts now...\n";
+    while (!window.shouldClose()) {
+      glfwPollEvents();
+    }
+    device->waitIdle();
 
   } catch (const std::exception &e) {
     std::cerr << e.what() << "\n";
