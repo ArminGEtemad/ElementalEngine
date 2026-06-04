d849dfab551fa2d741870d9b18d3fbe812996db2
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-06-04 09:53:06 +0100

    Added buffer boilerplates and the usage cases

---
+++ src/rhi/vulkan/VulkanBuffer.cpp
@@ -0,0 +1,63 @@
+#include "VulkanBuffer.hpp"
+#include "VulkanDevice.hpp"
+#include <stdexcept>
+namespace elementalEngine::RHI {
+VulkanBuffer::VulkanBuffer(VulkanDevice &device, size_t size, BufferUsage usage,
+                           MemoryProperty memoryProperty)
+    : device(device), size(size), usage(usage), memoryProperty(memoryProperty) {
+  VkBufferCreateInfo bufferInfo{VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO};
+  bufferInfo.size = size;
+
+  VkBufferUsageFlags vkUsage = 0;
+  if (usage & BufferUsage::Vertex)
+    vkUsage |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+  if (usage & BufferUsage::Index)
+    vkUsage |= VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+  if (usage & BufferUsage::Uniform)
+    vkUsage |= VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+  if (usage & BufferUsage::Storage)
+    vkUsage |= VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+  if (usage & BufferUsage::TransferSrc)
+    vkUsage |= VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+  if (usage & BufferUsage::TransferDst)
+    vkUsage |= VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+  bufferInfo.usage = vkUsage;
+  bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+  VmaAllocationCreateInfo allocInfo{};
+  if (memoryProperty == MemoryProperty::CPUAccess) {
+    allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
+    allocInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT;
+  } else {
+    allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
+    allocInfo.preferredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+  }
+
+  if (vmaCreateBuffer(device.getAllocator(), &bufferInfo, &allocInfo,
+                      &this->buffer, &this->allocation,
+                      nullptr) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to allocate VMA buffer!");
+  }
+}
+
+VulkanBuffer::~VulkanBuffer() {
+  if (this->buffer != VK_NULL_HANDLE) {
+    vmaDestroyBuffer(this->device.getAllocator(), this->buffer,
+                     this->allocation);
+  }
+}
+
+void *VulkanBuffer::map() {
+  void *mappedData = nullptr;
+  if (vmaMapMemory(this->device.getAllocator(), this->allocation,
+                   &mappedData) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to map Vulkan buffer memory via VMA!");
+  }
+  return mappedData;
+}
+
+void VulkanBuffer::unmap() {
+  vmaUnmapMemory(this->device.getAllocator(), this->allocation);
+}
+} // namespace elementalEngine::RHI
---
+++ src/rhi/vulkan/VulkanBuffer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Buffer.hpp"
+#include "RHICommon.hpp"
+#include "VulkanDevice.hpp"
+#include <cstddef>
+namespace elementalEngine::RHI {
+class VulkanBuffer : public Buffer {
+public:
+  VulkanBuffer(VulkanDevice &device, size_t size, BufferUsage usage,
+               MemoryProperty memoryProperty);
+  ~VulkanBuffer() override;
+
+  size_t getSize() const override { return size; };
+  BufferUsage getBufferUsage() const override { return usage; }
+  MemoryProperty getMemoryProperty() const override { return memoryProperty; }
+  VkBuffer getVkBuffer() const { return buffer; }
+  void *map() override;
+  void unmap() override;
+
+private:
+  VulkanDevice &device;
+  size_t size;
+  BufferUsage usage;
+  MemoryProperty memoryProperty;
+
+  VkBuffer buffer = VK_NULL_HANDLE;
+  VmaAllocation allocation = VK_NULL_HANDLE;
+};
+} // namespace elementalEngine::RHI
