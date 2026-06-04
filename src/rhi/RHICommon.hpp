956fa50e986522c91a83b84f8627ec29800ce52f
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-06-04 09:25:29 +0100

    Added buffer usage and test vertex
    I added the buffer usage class with 7 different use cases all in bitwise form to make sure that the I can combine them in an efficient way without using to much space

--- src/rhi/RHICommon.hpp
+++ src/rhi/RHICommon.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 namespace elementalEngine::RHI {
 enum class GraphicsAPI {
   Vulkan,
@@ -9,6 +10,35 @@ enum class GraphicsAPI {
 struct DeviceConfig {
   bool enableValidationLayers = true;
   bool enableGPUAssistedValidatioLayer = false;
+};
+
+// buffer usage
+enum class BufferUsage : uint32_t {
+  None = 0,
+  Vertex = 1 << 0,      // triangles
+  Index = 1 << 1,       // index array
+  Uniform = 1 << 2,     // uniform buffer for constants
+  Storage = 1 << 3,     // massive data
+  TransferSrc = 1 << 4, // source
+  TransferDst = 1 << 5, // destination
+};
+
+// combining buffer usages
+inline BufferUsage operator|(BufferUsage a, BufferUsage b) {
+  return (static_cast<BufferUsage>(static_cast<uint32_t>(a) |
+                                   static_cast<uint32_t>(b)));
+}
+inline bool operator&(BufferUsage a, BufferUsage b) {
+  return (static_cast<uint32_t>(a) & static_cast<uint32_t>(b));
+}
+
+// placed memory
+enum class MemoryProperty { GPULocal, CPUAccess };
+
+// test Vertex
+struct Vertex {
+  float position[2];
+  float color[3];
 };
 
 } // namespace elementalEngine::RHI
