c34dd54bdf91f05c8aaddf0db8e4da479f4c8f8d
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-06-06 20:31:58 +0100

    Added dispatch, compute pipeline contracts

--- src/rhi/CommandList.hpp
+++ src/rhi/CommandList.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Buffer.hpp"
+#include "ComputePipeline.hpp"
 #include "Pipeline.hpp"
 #include "Swapchain.hpp"
 #include <cstdint>
@@ -28,6 +29,11 @@ public:
   virtual void bindPipeline(Pipeline &pipeline) = 0;
   virtual void bindVertexBuffer(Buffer *buffer, size_t stride) = 0;
 
+  virtual void bindComputePipeline(ComputePipeline &pipeline) = 0;
+  virtual void bindStorageBuffer(uint32_t bindingSlot, Buffer *buffer) = 0;
+
+  virtual void dispatch(uint32_t groupCountX, uint32_t groupCountY,
+                        uint32_t groupCountZ) = 0;
   virtual void draw(uint32_t vertexCount, uint32_t instanceCount,
                     uint32_t firstVertex, uint32_t firstInstance) = 0;
 
