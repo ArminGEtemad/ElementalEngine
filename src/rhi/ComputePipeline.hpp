9dcbef63b7721259eb3f8c55efd80da95f4f9bf6
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-06-07 13:13:14 +0100

    Added grid and sim config to be pushed in shaders

--- src/rhi/ComputePipeline.hpp
+++ src/rhi/ComputePipeline.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <cstdint>
 namespace elementalEngine::RHI {
+struct SimConfig {
+  uint32_t gridWidth;
+  uint32_t gridHeight;
+  float dt;
+  float pad_0;
+};
 class ComputePipeline {
 public:
   virtual ~ComputePipeline() = default;
