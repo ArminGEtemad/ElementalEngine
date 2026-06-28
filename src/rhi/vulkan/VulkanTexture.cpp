2875ab3fc93e2757bbccffbc36786932a46e169e
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-06-28 11:43:36 +0100

    Added Texture contracts and boilerplates
    We are moving from Buffers to Textures so I can also use the hardware accelerated filters such as interpolations which I am doing right now in the shaders using ALU units myself.

---
+++ src/rhi/vulkan/VulkanTexture.cpp
@@ -0,0 +1,96 @@
+#include "VulkanTexture.hpp"
+#include "Texture.hpp"
+#include "VulkanDevice.hpp"
+#include <stdexcept>
+
+namespace elementalEngine::RHI {
+VulkanTexture::VulkanTexture(VulkanDevice &device, uint32_t width,
+                             uint32_t height, TextureFormat format,
+                             TextureUsage usage)
+    : device(device), width(width), height(height), format(format),
+      usage(usage) {
+
+  vkFormat = mapFormat(format);
+  VkImageUsageFlags vkUsage = mapUsage(usage);
+
+  VkImageCreateInfo imageInfo{};
+  imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+  imageInfo.imageType = VK_IMAGE_TYPE_2D;
+  imageInfo.extent.width = width;
+  imageInfo.extent.height = height;
+  imageInfo.extent.depth = 1;
+  imageInfo.mipLevels = 1;
+  imageInfo.arrayLayers = 1;
+  imageInfo.format = vkFormat;
+  imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+  imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+  imageInfo.usage = vkUsage;
+  imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+  imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+  VmaAllocationCreateInfo allocInfo{};
+  allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
+  allocInfo.preferredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+
+  if (vmaCreateImage(device.getAllocator(), &imageInfo, &allocInfo, &image,
+                     &allocation, nullptr) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to allocate texture via VMA!");
+  }
+
+  VkImageViewCreateInfo imageViewInfo{};
+  imageViewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+  imageViewInfo.image = image;
+  imageViewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+  imageViewInfo.format = vkFormat;
+  imageViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+  imageViewInfo.subresourceRange.baseMipLevel = 0;
+  imageViewInfo.subresourceRange.levelCount = 1;
+  imageViewInfo.subresourceRange.baseArrayLayer = 0;
+  imageViewInfo.subresourceRange.layerCount = 1;
+
+  if (vkCreateImageView(device.getLogicalDevice(), &imageViewInfo, nullptr,
+                        &imageView) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to create Vulkan Image View!");
+  }
+}
+
+// destroy
+VulkanTexture::~VulkanTexture() {
+  if (imageView != VK_NULL_HANDLE) {
+    vkDestroyImageView(device.getLogicalDevice(), imageView, nullptr);
+  }
+
+  if (image != VK_NULL_HANDLE) {
+    vmaDestroyImage(device.getAllocator(), image, allocation);
+  }
+}
+
+VkFormat VulkanTexture::mapFormat(TextureFormat format) {
+  switch (format) {
+  case TextureFormat::B8G8R8A8_SRGB:
+    return VK_FORMAT_B8G8R8A8_SRGB;
+  case TextureFormat::R32_FLOAT:
+    return VK_FORMAT_R32_SFLOAT;
+  case TextureFormat::R32G32_FLOAT:
+    return VK_FORMAT_R32G32_SFLOAT;
+  default:
+    throw std::runtime_error("Unsupported Texture format vulkan!");
+  }
+}
+
+VkImageUsageFlags VulkanTexture::mapUsage(TextureUsage usage) {
+  VkImageUsageFlags flags = 0;
+  if (usage & TextureUsage::ShaderResource)
+    flags |= VK_IMAGE_USAGE_SAMPLED_BIT;
+  if (usage & TextureUsage::UnorderedAccess)
+    flags |= VK_IMAGE_USAGE_STORAGE_BIT;
+  if (usage & TextureUsage::RenderTarget)
+    flags |= VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+  if (usage & TextureUsage::TransferSrc)
+    flags |= VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
+  if (usage & TextureUsage::TransferDst)
+    flags |= VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+  return flags;
+}
+
+} // namespace elementalEngine::RHI
---
+++ src/rhi/vulkan/VulkanTexture.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Texture.hpp"
+#include "VulkanDevice.hpp"
+#include <cstdint>
+
+namespace elementalEngine::RHI {
+class VulkanTexture : public Texture {
+public:
+  VulkanTexture(VulkanDevice &device, uint32_t width, uint32_t height,
+                TextureFormat format, TextureUsage usage);
+  ~VulkanTexture() override;
+
+  uint32_t getWidth() const override { return width; }
+  uint32_t getHeight() const override { return height; }
+  TextureFormat getFormat() const override { return format; }
+  TextureUsage getUsage() const override { return usage; }
+
+  VkImage getImage() const { return image; }
+  VkImageView getImageView() const { return imageView; }
+  VkFormat getVkFormat() const { return vkFormat; }
+
+private:
+  VulkanDevice &device;
+  uint32_t width;
+  uint32_t height;
+  TextureFormat format;
+  TextureUsage usage;
+
+  VkFormat vkFormat;
+  VkImage image{VK_NULL_HANDLE};
+  VmaAllocation allocation{VK_NULL_HANDLE};
+  VkImageView imageView{VK_NULL_HANDLE};
+
+  // helper functions
+  VkFormat mapFormat(TextureFormat format);
+  VkImageUsageFlags mapUsage(TextureUsage usage);
+};
+
+} // namespace elementalEngine::RHI
