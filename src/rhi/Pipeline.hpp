9472c8fdbbe96882377264315550bf5b209b2b5a
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-07-12 20:34:10 +0100

    Added th Clavet parameters for Goo
    These parameters are for now in the pipeline. I have to move them away from pipeline. but before something gets broken I needed a check point

--- src/rhi/Pipeline.hpp
+++ src/rhi/Pipeline.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "RHICommon.hpp"
+#include <cstdint>
 #include <vector>
+
+static constexpr uint32_t MAX_SPRINGS = 16;
 namespace elementalEngine::RHI {
 // Take this configuration structs to another scripts nect to the physics core
 // TODO change the name later to match the fact that this is only for the stam
@@ -11,25 +14,51 @@ struct SimConfig {
   uint32_t gridHeight;
   float dt;
   float forceY;
+  uint32_t numParticles;
+  uint32_t pad[3];
 };
 
 // paticle based fluid for the acidic slime projectile
 struct Particle {
   float position[2];
   float velocity[2];
+
   float predictedPosition[2];
-  uint32_t state; // 0: flying 1: settled (used to transition to the stam fluid)
-  float pad0;
+  float density;
+  float nearDensity;
+
+  float pressure;
+  float nearPressure;
+  uint32_t pad[2];
 };
 
 // particle simulation parameters
+// Clavet Paper
 struct ParticleSimulationParameters {
   float dt;
-  float restDensity;
-  float stiffness; // resist to compression
-  float viscosity;
-  float particleMass;
-  float smoothingRadius; // how far particles look for the neighbor
+  uint32_t numParticles;
+  float interactionRadius;  // h
+  float interactionRadius2; // h^2
+
+  float restDensity;     // rho0
+  float stiffness;       // k
+  float nearStiffness;   // k^near
+  float linearViscosity; // sigma
+
+  float quadraticViscosity; // beta
+  float springStiffness;    // k^spring
+  float plasticity;         // alpha
+  float yieldRatio;         // gamma
+
+  float sticknessRadius;
+  float sticknessMultiplier; // mu
+  float cellSpacing;
+  uint32_t hashGridSize;
+};
+
+struct Spring {
+  uint32_t neighborID; // INVALID_ID if empty
+  float restLength;
 };
 
 // -----------------------------------------------------------------------------
