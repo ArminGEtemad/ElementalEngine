4e4fbe92118a2a484941ebb3ed87f485c07fd168
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-08-12 20:00:16 +0100

    Added 2D terrain as ground for other elments

---
+++ src/graphics/TerrainPass.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Buffer.hpp"
+#include "Camera.hpp"
+#include "CommandList.hpp"
+#include "Device.hpp"
+#include "Pipeline.hpp"
+#include "Swapchain.hpp"
+#include "Texture.hpp"
+#include <memory>
+
+namespace elementalEngine::Graphics {
+
+class TerrainPass {
+public:
+  TerrainPass(RHI::Device &device, RHI::Swapchain &swapchain);
+  ~TerrainPass() = default;
+
+  // Updates camera matrix transformations
+  void update(WindowHandling &window, float deltaTime, float totalTime);
+
+  // Records 3D render pass commands into command list
+  void render(RHI::CommandList &commandList, RHI::Texture *targetColorTexture,
+              uint32_t width, uint32_t height);
+
+  // Recreates depth texture when window resolution changes
+  void onResize(uint32_t newWidth, uint32_t newHeight);
+
+  Core::Camera &getCamera() { return camera; }
+
+private:
+  void createDepthTarget(uint32_t width, uint32_t height);
+  void createBuffers();
+  void createPipeline();
+
+  uint32_t indexCount{0};
+
+  RHI::Device &device;
+  Core::Camera camera;
+
+  std::unique_ptr<RHI::Texture> depthTexture;
+  std::unique_ptr<RHI::Buffer> vertexBuffer;
+  std::unique_ptr<RHI::Buffer> indexBuffer;
+  std::unique_ptr<RHI::Buffer> cameraUniformBuffer;
+  std::unique_ptr<RHI::Pipeline> pipeline;
+};
+
+} // namespace elementalEngine::Graphics
