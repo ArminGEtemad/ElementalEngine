d5f0cd2c21eca89d93a157f9b67d46fbab43ab16
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-08-12 20:04:07 +0100

    Added normal for underneath the plane

--- shaders/Terrain_fs.hlsl
+++ shaders/Terrain_fs.hlsl
@@ -4,8 +4,12 @@ struct VSOutput {
   float2 uv : TEXCOORD0;
 };
 
-float4 FSMain(VSOutput input) : SV_Target {
-  float3 vecN = normalize(input.normal);
+float4 FSMain(VSOutput input, bool isFrontFace : SV_IsFrontFace) : SV_Target {
+
+  // normal shows downwards when looking the underneath the plane
+  float3 actualNormal = isFrontFace ? input.normal : -input.normal;
+
+  float3 vecN = normalize(actualNormal);
 
   // light point
   float3 lightDir = normalize(float3(1.5f, 1.5f, 1.5f));
@@ -13,12 +17,15 @@ float4 FSMain(VSOutput input) : SV_Target {
 
   // lambertian
   float3 ambient = float3(0.05f, 0.05f, 0.08f);
+
+  // Because the normal points DOWN on the back face, NdotL becomes 0.0
   float NdotL = max(dot(vecN, lightDir), 0.0f);
   float3 diffuse = NdotL * lightColor;
 
   // Base Terrain
   float3 terrainColor = float3(0.2f, 0.22f, 0.25f);
 
+  // Back face will now only receive ambient * terrainColor
   float3 finalColor = (ambient + diffuse) * terrainColor;
 
   return float4(finalColor, 1.0f);
