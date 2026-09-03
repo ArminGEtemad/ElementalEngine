5a1fce5d063997666a5a673770dfb2c897a067a4
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-09-03 13:01:42 +0100

    Added camera and terrain resource for raymarcher

--- src/graphics/StamFluidRenderer.cpp
+++ src/graphics/StamFluidRenderer.cpp
@@ -1,43 +1,74 @@
 #include "StamFluidRenderer.hpp"
 #include "Pipeline.hpp"
 #include "Texture.hpp"
+
 namespace elementalEngine::Renderer {
+
 StamFluidRenderer::StamFluidRenderer(RHI::Device &device) : device(device) {
   createRenderingPipeline();
 }
 
 void StamFluidRenderer::createRenderingPipeline() {
   using namespace RHI;
-  PipelineConfig GraphicPipelineConfig;
+  PipelineConfig config;
 
-  GraphicPipelineConfig.colorFormat = TextureFormat::B8G8R8A8_SRGB;
-  GraphicPipelineConfig.blendMode = Blendmode::Alpha;
-  GraphicPipelineConfig.depthState.depthTestEnable = false;
-  GraphicPipelineConfig.cullMode = CullMode::None;
-  GraphicPipelineConfig.bindings = {
-      {1, DescriptorType::SampledImage, 1, ShaderStage::Fragment},
-      {3, DescriptorType::SampledImage, 1, ShaderStage::Fragment}};
-  GraphicPipelineConfig.pushConstants.size = sizeof(Physics::SimConfig);
-  GraphicPipelineConfig.pushConstants.stage = ShaderStage::Fragment;
+  // because the raymarcher samples the depth it self there is no need for it in
+  // the configs
+  config.colorFormat = TextureFormat::B8G8R8A8_SRGB;
+  config.blendMode = Blendmode::Alpha;
+  config.depthState.depthTestEnable = false;
+  config.cullMode = CullMode::None;
 
-  graphicsPipeline =
-      device.createPipeline("grid_vs", "grid_fs", GraphicPipelineConfig);
+  config.bindings = {
+      {1, DescriptorType::SampledImage, 1,
+       ShaderStage::Fragment}, // Density (t1)
+      {2, DescriptorType::Sampler, 1,
+       ShaderStage::Fragment}, // Linear Sampler (s2)
+      {3, DescriptorType::SampledImage, 1,
+       ShaderStage::Fragment}, // Depth Texture (t3)
+  };
+
+  config.pushConstants.size = sizeof(GasRenderConfig);
+  config.pushConstants.offset = 0;
+  config.pushConstants.stage = ShaderStage::Fragment;
+
+  graphicsPipeline = device.createPipeline("grid_vs", "grid_fs", config);
 }
 
 void StamFluidRenderer::draw(RHI::CommandList &commandList,
                              const Physics::StamFluid &fluidSimulation,
-                             uint32_t screenWidth, uint32_t screenHeight) {
-  Physics::SimConfig configData = fluidSimulation.getSimConfig();
+                             const glm::mat4 &invViewProj,
+                             const glm::vec3 &cameraPos, uint32_t screenWidth,
+                             uint32_t screenHeight,
+                             RHI::Texture *terrainDepthTexture) {
+
+  // TODO start define these constants in one single script at some point!
+  float worldSizeX = 20.0f;
+  float worldSizeY = 20.0f;
+  float worldSizeZ = 20.0f;
+
+  GasRenderConfig renderConfig{};
+  renderConfig.invViewProj = invViewProj;
+  renderConfig.cameraPos = glm::vec4(cameraPos, 1.0f);
+
+  renderConfig.domainMin =
+      glm::vec4(-worldSizeX * 0.5f, 0.0f, -worldSizeZ * 0.5f, 1.0f);
+  renderConfig.domainMax =
+      glm::vec4(worldSizeX * 0.5f, worldSizeY, worldSizeZ * 0.5f, 1.0f);
 
   commandList.bindPipeline(*graphicsPipeline);
   commandList.setViewport(0.0f, 0.0f, static_cast<float>(screenWidth),
                           static_cast<float>(screenHeight));
   commandList.setScissor(0, 0, screenWidth, screenHeight);
-  commandList.pushConstants(0, sizeof(Physics::SimConfig), &configData,
-                            RHI::ShaderStage::Fragment);
-  commandList.bindTexture(1, fluidSimulation.getRenderTexture());
 
-  commandList.draw(3, 1, 0, 0); // Fullscreen Triangle
+  commandList.pushConstants(0, sizeof(GasRenderConfig), &renderConfig,
+                            RHI::ShaderStage::Fragment);
+
+  commandList.bindTexture(1, fluidSimulation.getRenderTexture());
+  commandList.bindSampler(2);
+  commandList.bindTexture(3, terrainDepthTexture);
+
+  commandList.draw(3, 1, 0, 0); // Fullscreen triangle
 }
 
 } // namespace elementalEngine::Renderer
--- src/graphics/StamFluidRenderer.hpp
+++ src/graphics/StamFluidRenderer.hpp
@@ -2,10 +2,19 @@
 #include "Device.hpp"
 #include "Pipeline.hpp"
 #include "StamFluid.hpp"
+#include <glm/glm.hpp>
 
 #include <memory>
 
 namespace elementalEngine::Renderer {
+
+struct GasRenderConfig {
+  glm::mat4 invViewProj;
+  glm::vec4 cameraPos;
+  glm::vec4 domainMin;
+  glm::vec4 domainMax;
+};
+
 class StamFluidRenderer {
 public:
   StamFluidRenderer(RHI::Device &device);
@@ -13,7 +22,9 @@ public:
 
   void draw(RHI::CommandList &commandList,
             const elementalEngine::Physics::StamFluid &fluidSimulation,
-            uint32_t screenWidth, uint32_t screenHeight);
+            const glm::mat4 &invViewProj, const glm::vec3 &cameraPos,
+            uint32_t screenWidth, uint32_t screenHeight,
+            RHI::Texture *terrainDepthTexture);
 
 private:
   RHI::Device &device;
